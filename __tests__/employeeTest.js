6a4ee86fb662cc2234ceb81c61b214f8161c29d8
Alix17 <wildflowerinabox@gmail.com>
2021-10-19 09:58:20 -0700

    test

--- __tests__/employeeTest.js
+++ __tests__/employeeTest.js
@@ -1,31 +1,46 @@
 //https://jestjs.io/docs/mock-functions
 
-const employeeTest = require('..employee.js');
+const employeeTest = require('../lib/Employee');
+
 
 describe('employee', () => {
-    it('should return the name, id, email', () => {
-        expect(employeeTest.getMockName()).toBe('a mock name');
-        expect(employeeTest.getMockid()).toBe('a mock id');
-        expect(employeeTest.getMockemail()).toBe('a mock email');
+    it('should return the name', () => {
+        const newEmployee = new employeeTest('greg')
+        console.log(newEmployee.getName())
+        expect(newEmployee.getName()).toEqual('greg')
     });
-    // class employee {
-    //     constructor(name, id, email) {
-    //         this.name = name;
-    //         this.id = id;
-    //         this.email = email;
-    //     }
-    //     getName() {
-    //         return this.name;
-    //     }
-    //     getId() {
-    //         return this.id;
-    //     }
-    //     getEmail() {
-    //         return this.email;
-    //     }
-    //     getRole() {
-    //         return 'Employee';
-    //     }
-    // }
 
-    console.log(employeeTest);
+    it('should return an id', () => {
+        const newEmployee = new employeeTest('greg', '3743')
+        console.log(newEmployee.getId())
+        expect(newEmployee.getId()).toEqual('3743')
+    })
+    it('should return an email', () => {
+        const newEmployee = new employeeTest('greg', '3743', 'me@me.com')
+        console.log(newEmployee.getEmail())
+        expect(newEmployee.getEmail()).toEqual('me@me.com')
+    })
+})
+
+
+// class employee {
+//     constructor(name, id, email) {
+//         this.name = name;
+//         this.id = id;
+//         this.email = email;
+//     }
+//     getName() {
+//         return this.name;
+//     }
+//     getId() {
+//         return this.id;
+//     }
+//     getEmail() {
+//         return this.email;
+//     }
+//     getRole() {
+//         return 'Employee';
+//     }
+// }
+
+console.log(employeeTest)
